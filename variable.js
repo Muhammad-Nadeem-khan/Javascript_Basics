77172f97d7ca411aa16bb522dbbbd56fc341b983
Muhammad-Nadeem-khan <145760243+Muhammad-Nadeem-khan@users.noreply.github.com>
2024-10-10 11:32:53 -0700

    Discuss vairables

---
+++ variable.js
@@ -0,0 +1,20 @@
+//*******************/ let , var ,const **********************//
+
+// variable : Memory location to store data.;
+
+// Programming rule and regulation:
+
+//1: Spaces in between operaters.
+
+//2: Variable name must be meaningfull.
+
+//3: Name must be in sanake case.
+
+//4: Variables names must be unique.
+
+//5:Variable names must not from number.
+
+//6: Keywords can not be used as a variable names.
+
+
+
